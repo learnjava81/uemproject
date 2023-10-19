2fb25f51d3fb7b87fd6c0e3543a82666a3c47f87
VishalJaiswal <vishal.mobile4391@gmail.com>
2023-10-19 12:52:15 +0530

    Adding new project

---
+++ oauth/src/main/java/com/example/uem/oauth/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.uem.oauth.repository;
+
+import com.example.uem.oauth.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
